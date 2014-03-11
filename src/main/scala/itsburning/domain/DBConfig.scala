4e611a261709a9929e0a061e9a119bbcfa4f207c
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2014-03-11 18:45:30 +0200

    - fixed typo in DBConfig

--- src/main/scala/itsburning/domain/DBConfig.scala
+++ src/main/scala/itsburning/domain/DBConfig.scala
@@ -12,5 +12,5 @@ trait TestDB extends DBConfig {
   val m = new DBModel("H2", new DAL(H2Driver), Database.forURL("jdbc:h2:mem:testdb", driver = "org.h2.Driver"))
   m.createDB
   m.addProduct(Product("Mild chili popper", 1, "It's pretty mild"))
-  m.addProduct(Product("Killer Nage Surprise", 5, "It will burn you to submission"))
+  m.addProduct(Product("Killer Naga Surprise", 5, "It will burn you to submission"))
 }
